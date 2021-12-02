7fa7bf9fd7a84719a1a034c5a131148d7254132c
Kyon Lee <kyon.lee@kogan.com.au>
2021-12-02 11:12:17 +1100

    initial commit

---
+++ 2021/01.js
@@ -0,0 +1,42 @@
+const { transforms } = require('advent-of-code-client');
+const { aocService } = require('../service/aocc');
+
+const client = aocService(2021, 1);
+
+client.setInputTransform(transforms.numbers);
+
+const increaseCount = (data) => {
+  let count = 0;
+  for (let i = 1; i < data.length; i += 1) {
+    if (data[i] > data[i - 1]) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+const summedInputs = (data) => {
+  const output = [];
+  for (let i = 0; i < data.length; i += 1) {
+    output.push(data[i] + data[i + 1] + data[i + 2]);
+  }
+  return output;
+};
+
+const part1 = (data) => {
+  console.log(increaseCount(data));
+};
+
+const part2 = (data) => {
+  console.log(increaseCount(summedInputs(data)));
+};
+
+const main = async () => {
+  const input = await client.getInput();
+  part1(input);
+  part2(input);
+};
+
+if (require.main === module) {
+  main();
+}
